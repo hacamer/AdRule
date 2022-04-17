eb79a498dadd9a7dc2393047e7532fbb2cd072dd
杏梢 <73057402+hacamer@users.noreply.github.com>
2022-04-17 09:45:04 +0800

    Update clean-versions.sh

--- clean-versions.sh
+++ clean-versions.sh
@@ -3,5 +3,5 @@ time=$(TZ=UTC-8 date +'%Y-%m-%d %H:%M')
 date=$(TZ=UTC-8 date +'%Y-%m-%d %H:%M:%S')（北京时间）
 sed -i "s/! Version:.*/! Version: $time /g" rules-admin.txt url-filter.txt
 sed -i "s/! Last Update:.*/! Last Update: $date /g" rules-admin.txt url-filter.txt
-cat rules-admin.txt | grep -E "^[(\@\@)|(\|\|)][^\/\^]+\^$" |sort | uniq > dns.txt
+cat rules-admin.txt | grep -E "^[(\@\@)|(\|\|)][^\/\^]+\^" |sort | uniq > dns.txt
 exit

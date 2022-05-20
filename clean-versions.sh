d221ddd9d4c905b587a677ca28404bf766f5db06
杏梢 <73057402+hacamer@users.noreply.github.com>
2022-05-20 20:30:44 +0800

    Update clean-versions.sh

--- clean-versions.sh
+++ clean-versions.sh
@@ -3,5 +3,28 @@ time=$(TZ=UTC-8 date +'%Y-%m-%d %H:%M')
 date=$(TZ=UTC-8 date +'%Y-%m-%d %H:%M:%S')（北京时间）
 sed -i "s/! Version:.*/! Version: $time /g" rules-admin.txt url-filter.txt
 sed -i "s/! Last Update:.*/! Last Update: $date /g" rules-admin.txt url-filter.txt
+
+easylist=(
+  "https://raw.githubusercontent.com/hoshsadiq/adblock-nocoin-list/master/nocoin.txt"
+  "https://raw.githubusercontent.com/crazy-max/WindowsSpyBlocker/master/data/hosts/spy.txt"
+  "https://raw.githubusercontent.com/banbendalao/ADgk/master/ADgk.txt"
+  "https://easylist.to/easylist/easyprivacy.txt"
+  "https://filters.adtidy.org/windows/filters/224.txt"
+  "https://raw.githubusercontent.com/damengzhu/banad/main/jiekouAD.txt"
+)
+for i in "${!easylist[@]}"
+do
+  echo "开始下载 easylist${i}..."
+  curl -k -o "./easylist${i}.txt" --connect-timeout 60 -s "${easylist[$i]}" &
+  # shellcheck disable=SC2181
+  if [ $? -ne 0 ];then
+    echo '下载失败，请重试'
+    exit 1
+  fi
+done
+wait
+
 cat rules-admin.txt | grep -E "^[(\@\@)|(\|\|)][^\/\^]+\^" | grep -Fv "$" |sort | uniq > dns.txt
+cat easy* dns.txt | grep -E "^[(\@\@)|(\|\|)][^\/\^]+\^$" |sort | uniq > dns-list.txt
+rm -f easy*
 exit
